f2b66892b286704bf9c0a9d5a52f087ce73032b6
Jeffrey Davis <j.davis081248@gmail.com>
2023-11-29 13:19:09 -0600

    fixed practice1 branch?

--- src/index.js
+++ src/index.js
@@ -1,83 +1,145 @@
 const url = "http://localhost:3000/ramens"
 
-const ramenMenuElement = document.querySelector("#ramen-menu")
+const navMenu = document.querySelector("#ramen-menu")
 
-const detailImageElement = document.querySelector("div#ramen-detail img.detail-image")
-const nameElement = document.querySelector("div#ramen-detail h2.name")
-const restaurantElement = document.querySelector("div#ramen-detail h3.restaurant")
-const ratingDisplayElement = document.querySelector("#rating-display")
-const commentDisplayElement = document.querySelector("#comment-display")
+const detailImg = document.querySelector("div#ramen-detail img.detail-image")
+const detailName = document.querySelector("div#ramen-detail h2.name")
+const detailRest = document.querySelector("div#ramen-detail h3.restaurant")
+const ratingDisplay = document.querySelector("#rating-display")
+const commentDisplay = document.querySelector("#comment-display")
 
-const newRamenFormElement = document.querySelector("#new-ramen")
-const formNameElement = document.querySelector("form#new-ramen input#new-name")
-const formRestaurantElement = document.querySelector("form#new-ramen input#new-restaurant")
-const formImageElement = document.querySelector("form#new-ramen input#new-image")
-const formRatingElement = document.querySelector("form#new-ramen input#new-rating")
-const formCommentElement = document.querySelector("form#new-ramen input#new-rating")
+const newRamenForm = document.querySelector("#new-ramen")
+const nameInput = document.querySelector("form#new-ramen input#new-name")
+const restInput = document.querySelector("form#new-ramen input#new-restaurant")
+const imgInput = document.querySelector("form#new-ramen input#new-image")
+const ratingInput = document.querySelector("form#new-ramen input#new-rating")
+const commentInput = document.querySelector("form#new-ramen textarea#new-comment")
 
-let ramenList
+const editForm = document.querySelector("#edit-ramen")
+const updatedRating = document.querySelector("form#edit-ramen input#new-rating")
+const updatedComment = document.querySelector("form#edit-ramen textarea#new-comment")
 
-function menu(url)
+let ramenCopy
+let currentRamen
+
+function getRamen()
 {
     fetch(url)
-        .then (resp => resp.json())
-        .then (ramenData => {
-            displayRamenMenu(ramenData)
+        .then(resp => resp.json())
+        .then(ramenData => 
+            {
+                ramenCopy = ramenData
+                createNavBar(ramenData)
+                displayDetails(ramenData[0])
+                addRamen()
+                updateRamen()
+                deleteRamen()
+            })
+}
 
-            displayRamenDetails(ramenData[0])
+function createNavBar(ramens)
+{
+    navMenu.innerText = ""
+    ramens.forEach(ramen => 
+        {
+            displayImg = document.createElement("img")
+            displayImg.src = ramen.image
+            navMenu.appendChild(displayImg)
 
-            createNewRamen()
+            displayImg.addEventListener("click", () => displayDetails(ramen))
         })
 }
 
-function displayRamenMenu(ramens)
+function displayDetails(ramen)
 {
-    if (Array.isArray(ramens))
-    {
-        ramens.forEach(ramen => {
-            const ramenMenuImageElement = document.createElement("img")
-            ramenMenuImageElement.src = ramen.image
-            ramenMenuElement.appendChild(ramenMenuImageElement)
-    
-            ramenMenuImageElement.addEventListener("click", () => displayRamenDetails(ramen))
-            })
-    }
-    
-    else
-    {
-        const ramenMenuImageElement = document.createElement("img")
-        ramenMenuImageElement.src = ramens.image
-        ramenMenuElement.appendChild(ramenMenuImageElement)
+    currentRamen = ramen
 
-        ramenMenuImageElement.addEventListener("click", () => displayRamenDetails(ramens))
-    }
+    detailImg.src = ramen.image
+    detailName.innerText = ramen.name
+    detailRest.innerText = ramen.restaurant
+    ratingDisplay.innerText = ramen.rating
+    commentDisplay.innerText = ramen.comment
 }
 
-function displayRamenDetails(ramen)
+function addRamen()
 {
-    detailImageElement.src = ramen.image
-    nameElement.innerText = ramen.name
-    restaurantElement.innerText = ramen.restaurant
-    ratingDisplayElement.innerText = ramen.rating
-    commentDisplayElement.innerText = ramen.comment
-}
+    newRamenForm.addEventListener("submit", (e) => 
+    {
+        e.preventDefault()
 
-function createNewRamen()
-{
-    newRamenFormElement.addEventListener("submit", event => {
-        event.preventDefault()
-
-        const newRamen = {
-            name: formNameElement.value,
-            restaurant: formRestaurantElement.value,
-            image: formImageElement.value,
-            rating: formRatingElement.value,
-            comment: formCommentElement.value
+        const newRamen = 
+        {
+            name: nameInput.value,
+            restaurant: restInput.value,
+            image: imgInput.value,
+            rating: ratingInput.value,
+            comment: commentInput.value
         }
 
-        displayRamenMenu(newRamen)
-        newRamenFormElement.reset()
+        ramenCopy.push(newRamen)
+        createNavBar(ramenCopy)
+
+        newRamenForm.reset()
     })
 }
 
-menu(url)
+function updateRamen()
+{
+    editForm.addEventListener("submit", (e) => 
+    {
+        e.preventDefault()
+
+        currentRamen.rating = updatedRating.value
+        currentRamen.comment = updatedComment.value
+
+        ramenCopy.forEach(ramen => 
+            {
+                if(currentRamen.name === ramen.name)
+                {
+                    ramen.rating = updatedRating.value
+                    ramen.comment = updatedComment.value
+                }
+            })
+
+        createNavBar(ramenCopy)
+        displayDetails(currentRamen)
+
+        editForm.reset()
+    })
+}
+
+function deleteRamen()
+{
+    const deleteButton = document.createElement("input")
+    deleteButton.type = "submit"
+    deleteButton.value = "Delete Ramen"
+    deleteButton.style.background = "#DB1200"
+    document.body.appendChild(deleteButton)
+    const ramenCopy2 = []
+
+
+    deleteButton.addEventListener("click", () => 
+    {
+        for(let i = 0; i < ramenCopy.length(); i++)
+        {
+            if (ramenCopy[i] === currentRamen)
+            {
+                
+            }
+        }
+        // ramenCopy.forEach(ramen => 
+        // {
+        //     if(ramen.name !== currentRamen.name && !ramenCopy2.includes(currentRamen))
+        //     {
+        //         ramenCopy2.push(ramen)
+        //     }
+
+        //     else if (ramen.name === currentRamen.name)
+        // })
+
+        createNavBar(ramenCopy2)
+        displayDetails(ramenCopy2[0])
+    })
+}
+
+getRamen()
